913ecdd8963c5019926678ffd7aaa23d3dd6cac3
AnaBajo <kozicana@yahoo.com>
2023-03-01 11:34:10 +0100

    life hooks

--- components/LifeHooksComponent.vue
+++ components/LifeHooksComponent.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    <h1 class="m-5 text-center" >{{ propertyComputed }}</h1>
+  <div class="text-center">
+    <h1 v-if="showElement" class="m-5 text-center" ref="example-element" >{{ propertyComputed }}</h1>
+    <button @click="showElement = !showElement" type="button" class="btn btn-primary m-2">toggle button</button>
+    <p class="m-3">beforeUpdate(): {{ counter }}</p>
+    <p>{{ deleteProperty }}</p>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      property: 'Example property.'
+      property: 'initial example property value',
+      showElement: true,
+      counter: 0,
+      deleteProperty: 'remove me!'
     }
   },
   computed: {
@@ -16,22 +22,43 @@ export default {
     }
   },
   beforeCreate() {
-    // The beforeCreate hook runs at the initialization of your component - data has not been made reactive, and events have not been set up yet:
-    console.log('At this point, events and lifecycle have been initialized.')
+    // At this point, events and lifecycle have been initialized.
+    // console.log('beforeCreate() hook runs at the initialization of your component - data has not been made reactive, and events have not been set up yet')
   },
   created() {
-    // The created hook runs before the templates and Virtual DOM have been mounted or rendered - you are able to access reactive data and events that are active:
-    console.log('At this point, this.property is now reactive and propertyComputed will update.')
-    this.property = 'Example property updated.'
+    // At this point, this.property is now reactive and propertyComputed will update.
+    // console.log('created() hook runs before the templates and Virtual DOM have been mounted or rendered - you are able to access reactive data and events that are active')
+    this.property = 'data property updated in the created() hook'
+
+    setInterval(() => {
+      this.counter++
+    }, 1000)
   },
   beforeMount() {
-    // The beforeMount hook runs right before the initial render happens and after the template or render functions have been compiled:
-    console.log(`At this point, vm.$el has not been created yet.`)
+    // At this point, vm.$el has not been created yet.
+    // console.log(`beforeMount() hook runs right before the initial render happens and after the template or render functions have been compiled`)
   },
   mounted() {
-    // Use mounted for modifying the DOM—particularly when integrating non-Vue libraries:
-    console.log(`At this point, vm.$el has been created and el has been replaced.`);
-    console.log(this.$el.textContent) // Example component.
+    // At this point, vm.$el has been created and el has been replaced.
+    // console.log(`Use mounted() for modifying the DOM—particularly when integrating non-Vue libraries`);
+    console.log(`this.$el.textContent: ${ this.$el.textContent}`) // Example component.
+  },
+  beforeUpdate() {
+    // At this point, Virtual DOM has not re-rendered or patched yet.
+    // console.log(`beforeUpdate() logs the counter value every second, before the DOM updates. `)
+    // console.log(this.counter)
+  },
+  updated() {
+    // Fired every second, should always be true
+    // console.log(`At this point, Virtual DOM has re-rendered and patched.`)
+    // console.log(+this.$refs['example-element'].textContent === this.counter)
+  },
+  beforeDestroy() {
+    console.log(`At this point, watchers, child components, and event listeners have not been teared down yet.`)
+    // Perform the teardown procedure for deleteProperty.
+    // (In this case, effectively nothing)
+    this.deleteProperty = null
+    delete this.deleteProperty
   }
 }
 </script>
